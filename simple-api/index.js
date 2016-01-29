78ba19e8483df341a599d5a05f6f5ce1b32594e9
Konstantin Baierer <konstantin.baierer@bib.uni-mannheim.de>
2016-01-29 13:30:01 +0100

    Added simple cache layer
    Signed-off-by: Konstantin Baierer <konstantin.baierer@bib.uni-mannheim.de>

--- simple-api/index.js
+++ simple-api/index.js
@@ -5,43 +5,56 @@ var app = new express();
 var ZOTERO_BASEURL="http://localhost:1234";
 var SESSION_ID="foo-123";
 
+var cache = {};
 
 app.get('/', function(req, res, next) {
-  if (!req.query.url) {
-    return next("ERROR: Must specify url");
-  }
-  var format = 'ris';
-  if (req.query.format) {
-    format = req.query.format;
+  var url = req.query.url;
+  if (!url) return next("ERROR: Must specify url");
+  var format = req.query.format || 'ris';
+
+  cache[url] = cache[url] || {};
+  if (cache[url] && cache[url][format]) {
+    var cached = cache[url][format];
+    res.statusCode = cached[0];
+    res.set('Content-Type',  cached[1]);
+    res.send(cached[2]);
+    return;
   }
   request
       .post(ZOTERO_BASEURL + "/web")
-      .send({"url":req.query.url, "sessionid":SESSION_ID})
-      .end(function(err, resp){
-        if (err) return next(err);
+      .send({"url" : url, "sessionid" : SESSION_ID})
+      .end(function(err, resp) {
+        if (err) {
+          cache[url][doiMode ? 'doi' : format] = [500, 'text/plain', err.toString()];
+          return next(err);
+        };
         doiMode = format === 'doi';
-        if (doiMode) {
-          format = 'ris';
-        }
+        if (doiMode) format = 'ris';
         request
             .post(ZOTERO_BASEURL + "/export?format=" + format)
             .buffer(true)
             .send(resp.body)
             .end(function(err, resp) {
-              if (err) return next(err);
+              if (err) {
+                cache[url][doiMode ? 'doi' : format] = [500, 'text/plain', err.toString()];
+                return next(err);
+              };
+              var contentType = doiMode ? 'text/plain' : resp.headers['content-type'];
+              var statusCode = resp.statusCode;
+              var text = resp.text;
               if (doiMode) {
-                doiMatch = resp.text.match(/DO\s+-\s+(.*)/);
-                if (! doiMatch) {
-                  res.statusCode = 404;
-                  res.end();
+                m = resp.text.match(/DO\s+-\s+(.*)/);
+                if (! m) {
+                  statusCode = 404;
                 } else {
-                  res.set('Content-Type', "text/plain");
-                  res.send(doiMatch[1]);
+                  text = m[1];
                 }
-              } else {
-                res.set('Content-Type', resp.get('Content-Type'))
-                res.send(resp.text);
+                format = 'doi';
               }
+              var cached = cache[url][format] = [statusCode, contentType, text];
+              res.statusCode = cached[0];
+              res.set('Content-Type',  cached[1]);
+              res.send(cached[2]);
             });
       });
 });
