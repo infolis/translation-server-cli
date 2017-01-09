b13d4b4e9b307ce73efb447900551ba4e155cbdc
Konstantin Baierer <konstantin.baierer@bib.uni-mannheim.de>
2017-01-09 13:25:14 +0100

    caching and restart endpoint for simple api

--- simple-api/index.js
+++ simple-api/index.js
@@ -2,10 +2,15 @@ var express = require('express');
 var request = require('superagent');
 var app = new express();
 
-var ZOTERO_BASEURL="http://localhost:1234";
-var SESSION_ID="foo-123";
+var ZOTERO_BASEURL = "http://localhost:1234";
+var SESSION_ID = "foo-123";
+var CACHE_ENABLED = false;
 
 var cache = {};
+
+app.get('/restart', function() {
+  process.exit(10);
+});
 
 app.get('/', function(req, res, next) {
   var url = req.query.url;
@@ -13,22 +18,27 @@ app.get('/', function(req, res, next) {
   var format = req.query.format || 'ris';
 
   cache[url] = cache[url] || {};
-  if (cache[url] && cache[url][format]) {
+  if (CACHE_ENABLED && cache[url] && cache[url][format]) {
     var cached = cache[url][format];
+    console.log("In cache:  " + format + " / " + url);
     res.statusCode = cached[0];
     res.set('Content-Type',  cached[1]);
     res.send(cached[2]);
     return;
   }
+  console.log("Passing to zotero translation server: " + url);
   request
       .post(ZOTERO_BASEURL + "/web")
       .send({"url" : url, "sessionid" : SESSION_ID})
       .end(function(err, resp) {
+        var doiMode = format === 'doi';
         if (err) {
           cache[url][doiMode ? 'doi' : format] = [500, 'text/plain', err.toString()];
           return next(err);
-        };
-        doiMode = format === 'doi';
+        }
+        console.log("==============================================================================");
+        console.log(resp.body);
+        console.log("==============================================================================");
         if (doiMode) format = 'ris';
         request
             .post(ZOTERO_BASEURL + "/export?format=" + format)
@@ -38,12 +48,12 @@ app.get('/', function(req, res, next) {
               if (err) {
                 cache[url][doiMode ? 'doi' : format] = [500, 'text/plain', err.toString()];
                 return next(err);
-              };
+              }
               var contentType = doiMode ? 'text/plain' : resp.headers['content-type'];
               var statusCode = resp.statusCode;
               var text = resp.text;
               if (doiMode) {
-                m = resp.text.match(/DO\s+-\s+(.*)/);
+                var m = resp.text.match(/DO\s+-\s+(.*)/);
                 if (! m) {
                   statusCode = 404;
                 } else {
@@ -60,6 +70,7 @@ app.get('/', function(req, res, next) {
 });
 
 app.use(function errorHandler(err, req, res, next) {
-  res.status(406).send(err);
+    console.log("Failed request", err);
+    res.status(401).send(err);
 });
 app.listen(2000);
